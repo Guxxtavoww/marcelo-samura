69d455b0c8b23a7496d064c282e0454a54797ccc
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-10-08 15:03:19 -0300

    Chore: adding stuff

--- src/app/_containers/will-it-work-for-me.tsx
+++ src/app/_containers/will-it-work-for-me.tsx
@@ -6,9 +6,9 @@ export function WillItWorkForMe() {
       <h2 className="text-[#264653] text-4xl font-bold">
         A MENTORIA SAMURA FUNCIONA PRA MIM?
       </h2>
-      <p className="text-lg font-medium">
+      <p className="text-lg font-medium text-center">
         A Mentoria Samura serve para qualquer pessoa que deseja dominar as
-        ferramentas de autoconhecimento e sobre a espiritualidade para
+        ferramentas de <br /> autoconhecimento e sobre a espiritualidade para
         transformar a sua vida.
       </p>
     </section>
