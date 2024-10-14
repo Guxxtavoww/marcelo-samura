be3979264cb6480dc1e4491b38847e1fc4294142
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-10-14 16:54:00 -0300

    Chore: testemonials done

--- src/app/_containers/testemonials.tsx
+++ src/app/_containers/testemonials.tsx
@@ -1,19 +1,145 @@
 'use client';
 
+import Image from 'next/image';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import {
+  testemonialsData,
+  type TestemonialData,
+} from '@/data/testemonials.data';
+import { cn } from '@/utils/cn.util';
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from '@/components/ui/collapsible';
+import { Button } from '@/components/ui/button';
+
+function Testemonial({
+  location,
+  name,
+  portraitSrc,
+  profession,
+  testemonial,
+  isLastItem,
+}: TestemonialData & { isLastItem?: boolean }) {
+  const [isNotCollapsed, setIsNotCollapsed] = useState(false);
+
+  return (
+    <Collapsible
+      open={isNotCollapsed}
+      onOpenChange={setIsNotCollapsed}
+      className={cn(
+        'flex flex-col gap-2.5 items-center p-3 rounded-md border',
+        {
+          'col-span-2': isLastItem,
+        }
+      )}
+    >
+      <motion.div
+        whileInView={{ opacity: [0, 1], y: [20, 0] }} // Apply framer motion animation
+        transition={{ duration: 0.5 }}
+      >
+        <Image
+          src={portraitSrc}
+          alt={name}
+          width={200}
+          height={100}
+          className="rounded-md shadow-lg w-[200px] h-[150px]"
+        />
+      </motion.div>
+      <motion.h4
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="font-bold text-lg"
+      >
+        {name}
+      </motion.h4>
+      <motion.p
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        {profession}
+      </motion.p>
+      <motion.p
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        {location}
+      </motion.p>
+      {testemonial.length > 151 ? (
+        <>
+          {!isNotCollapsed ? (
+            <motion.p
+              whileInView={{ opacity: [0, 1], y: [20, 0] }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              className="leading-10 text-base font-medium"
+            >
+              {testemonial.substring(0, 151)}...
+            </motion.p>
+          ) : null}
+          <CollapsibleContent>
+            <motion.p
+              whileInView={{ opacity: [0, 1], y: [20, 0] }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              className="leading-10 text-base font-medium"
+            >
+              {testemonial}
+            </motion.p>
+          </CollapsibleContent>
+          <CollapsibleTrigger asChild>
+            <Button>{!isNotCollapsed ? 'Ler Mais' : 'Ler Menos'}</Button>
+          </CollapsibleTrigger>
+        </>
+      ) : (
+        <motion.p
+          whileInView={{ opacity: [0, 1], y: [20, 0] }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          className="leading-10 text-base font-medium"
+        >
+          {testemonial}
+        </motion.p>
+      )}
+    </Collapsible>
+  );
+}
+
 export function Testemonials() {
   return (
     <section
       className="container-wrapper text-center space-y-4"
       id="testemonials"
     >
-      <h2 className="section-heading">
+      <motion.h2
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5 }}
+        className="section-heading"
+      >
         Veja o que dizem sobre a Mentoria Samura:
-      </h2>
-      <h3 className="text-xl font-bold">
+      </motion.h2>
+      <motion.h3
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="text-2xl font-bold"
+      >
         Contra fatos reais não há argumentos, confira abaixo os resultados
         <br />
         dos membros da Mentoria Samura espalhados por todo o Brasil.
-      </h3>
+      </motion.h3>
+      <motion.div
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="w-full grid grid-cols-3 gap-4 max-lg:grid-cols-2 max-md:grid-cols-1"
+      >
+        {testemonialsData.map((testemonial, index, arr) => (
+          <Testemonial
+            {...testemonial}
+            isLastItem={arr.length - 1 === index}
+            key={index}
+          />
+        ))}
+      </motion.div>
     </section>
   );
 }
---
+++ src/data/testemonials.data.ts
@@ -0,0 +1,98 @@
+export interface TestemonialData {
+  name: string;
+  profession: string;
+  location: string;
+  testemonial: string;
+  portraitSrc: string;
+}
+
+export const testemonialsData: TestemonialData[] = [
+  {
+    name: 'Irlen Benchimol',
+    profession: 'Servidora Pública',
+    location: 'Manaus/AM',
+    testemonial:
+      '"A mentoria foi um canal de transformação na minha vida. Com o apoio do Marcello Samura, descobri meu verdadeiro potencial e alinhamento espiritual, ganhando clareza e confiança para seguir meu propósito. Essa jornada me trouxe uma profunda conexão interior e um novo sentido para minha vida."',
+    portraitSrc: '/irlen-benchimol.png',
+  },
+  {
+    name: 'Elaine Duailibi',
+    profession: 'Servidora Pública',
+    location: 'Vitória/ES',
+    testemonial:
+      '"O seu acompanhamento diário, especialmente nos momentos mais críticos, foi essencial para que eu superasse uma fase difícil da minha vida. Isso transformou minha visão sobre o campo da espirituualidade e sobre mim mesmo."',
+    portraitSrc: '/elaine-duailibi.png',
+  },
+  {
+    name: 'Patrícia Bezerra',
+    profession: 'Gerente de Vendas',
+    location: 'São Paulo/SP',
+    testemonial:
+      'A minha vida foi completamente transformada. Enfrentava crises no relacionamento, no que refletia diretamente no meu trabalho. A Mentoria Samura, foi recomendada por uma amiga e me ajudou a ver a necessidade de mudanças em várias áreas da minha vida. As conversas com o mentor, orações e a compreensão dos meus problemas e com soluções práticas, fizeram toda a diferença para mim e minha família. Sou profundamente grata a Marcello Samura.',
+    portraitSrc: '/patricia-bezerra.png',
+  },
+  {
+    name: 'Paulo Gabrioti',
+    profession: 'Empresário',
+    location: 'Espírito Santo do Pinhal/SP',
+    testemonial:
+      '"O seu trabalho fez toda a diferença em minha vida e da minha família, ele vivenciou o problema comigo o tempo todo, me mostrando soluções práticas para eu tomar as decisões que necessitava."',
+    portraitSrc: '/paulo-gabrioti.png',
+  },
+  {
+    name: 'Simone Capasso',
+    profession: 'Bancária',
+    location: 'São Paulo/SP',
+    testemonial:
+      '"A sua experiência e a sensibilidade em compreender e vivenciar comigo a dor, me de segurança para compreender que a solução já estava dentro de mim. Essa descoberta me guiou em uma jornada transformadora de conexão interior, despertando um poder que eu nem sabia que existia."',
+    portraitSrc: '/simone-capasso.png',
+  },
+  {
+    name: 'Luziane Tonhá Cardoso',
+    profession: 'Advogada',
+    location: 'Salvador/BA',
+    testemonial:
+      '“Foi um despertar de consciência, que me fez alcançar resultados expressivos em minha vida, profissional e principalmente no campo da espiritualidade”.',
+    portraitSrc: '/luziane-tonha-cardoso.png',
+  },
+  {
+    name: 'Janaina Chiovetto',
+    profession: 'Publicitária',
+    location: 'São Paulo/SP',
+    testemonial:
+      '"A mentoria transformou minha percepção sobre os caminhos da espiritualidade e o contato diário fez toda a diferença no processo de sair dos problemas que eu estava enfrentando. Sou muito grato ao Marcello, o seu trabalho foi um grande diferencial para a minha mudança , hoje reencontrei minha paz interior, alinhei meus valores com o campo da espiritualidade e vivo em paz.”',
+    portraitSrc: '/ticanaina.png',
+  },
+  {
+    name: 'Vitor Chiovetto',
+    profession: 'T.I',
+    location: 'São Paulo/SP',
+    testemonial:
+      'A minha vida transformou profundamente. Me abriu uma percepção diferente sobre tudo que vivia, me reconectei com minha essência, encontrei paz interior e clareza sobre meu propósito de vida. Essa experiência mudou não só minha perspectiva, mas também todo o meu caminho ao longo da jornada da vida, Gratidão Samura pelo trabalho incrível.',
+    portraitSrc: '/lelliskit.png',
+  },
+  {
+    name: 'Duda Bench',
+    profession: 'Representante Comercial',
+    location: 'Campinas /SP',
+    testemonial:
+      'A minha vida foi completamente transformada. Enfrentava crises no relacionamento, brigas, discussões, confusões, não tinha mais vontade de fazer nada e já estava considerando uma separação. Depois de 1 ano de conversas diárias, orações e uma e compreensão de que eu deveria mudar, fizeram toda a diferença para mim e minha família. Sou profundamente grata a Marcello Samura.',
+    portraitSrc: '/duda-bench.png',
+  },
+  {
+    name: 'Romilda Mazzili',
+    profession: 'Servidora Pública',
+    location: 'Guaxupé /MG',
+    testemonial:
+      '"Conheci o Marcelo em um dos momentos mais tristes da minha vida, quando perdi minha amada irmã para um câncer agressivo. Estava completamente perdida, e ele foi um verdadeiro bálsamo para minha alma. Sou grata por todas as lições valiosas que ele me ensinou e por ter me ajudado a crescer como pessoa. Sua sabedoria é um presente que levarei comigo para sempre."',
+    portraitSrc: '/romilda-mazzili.png',
+  },
+  {
+    name: 'Corah MullerWol',
+    profession: 'Pedagoga',
+    location: 'Leipzig /Alemanha',
+    testemonial:
+      '"A mentoria foi fundamental para me libertar de um casamento tóxico e violento. Com o apoio recebido, recuperei minha autoestima, encontrei minha força interior e consegui reconstruir minha vida com segurança e confiança. Hoje, estou em paz e pronta para seguir em frente, finalmente livre dessa relação destrutiva."',
+    portraitSrc: '/corah-mullerwol.png',
+  },
+];
